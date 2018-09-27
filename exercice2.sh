2e0f2113074c9eb3962aeabba2a66cc3be4e6046
Uotap <patoupointcom@gmail.com>
2018-09-27 17:04:20 -0400

    exercice3

--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+
+afficheMenu() {
+	clear
+	echo ""
+	echo "-----------------"
+	echo " d = ditionnaire "
+	echo " q = quitter     "
+	echo "-----------------"
+	echo ""
+}
+
+choix=a
+while test $choix != q -a $choix != d; do
+	afficheMenu
+	read -p "Faite votre choix : " choix
+	if [[ $choix =  d]];then
+		read -p "entrer un mot : " mot
+		ls=./exercice2.sh
+done
+
--- exercice3.sh
+++ exercice3.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+string="\t"
+string="@ $string"
+for((i=0;i<5;i++))
+do
+	#string="$string @\t
+
+	echo -ne $string
+	sleep 1
+	clear
+done
+
+echo "BOOM"
